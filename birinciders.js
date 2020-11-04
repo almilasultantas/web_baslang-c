c0377033020619ac34a50ee2dfdff27138ca2410
Almıla Sultan TAŞ <65083331+almilasultantas@users.noreply.github.com>
2020-11-04 04:02:49 +0300

    Add files via upload

---
+++ birinciders.js
@@ -0,0 +1,72 @@
+// hamburger = "hamburger"
+// drink = "Cola"
+// menu = hamburger + " " + drink
+// console.log(menu)
+// variables
+// var hamburger = "hamburger"
+// console.log(hamburger)
+// functions
+// var sayi1 = Math.ceil(Math.random() * 49)
+// var sayi2 = Math.ceil(Math.random() * 49)
+// var sayi3 = Math.ceil(Math.random() * 49)
+// var sayi4 = Math.ceil(Math.random() * 49)
+// var sayi5 = Math.ceil(Math.random() * 49)
+// var sayi6 = Math.ceil(Math.random() * 49)
+// console.log("Kolon: " + sayi1 + " " + sayi2 + " " + sayi3 + " " + sayi4 + " " + sayi5 + " " + sayi6)
+
+
+// function sayiUret() {
+    // return Math.ceil(Math.random() * 49)
+// }
+
+// var sayi1 = sayiUret()
+// var sayi2 = sayiUret()
+// var sayi3 = sayiUret()
+// var sayi4 = sayiUret()
+// var sayi5 = sayiUret()
+// var sayi6 = sayiUret()
+// console.log("Kolon: " + sayi1 + " " + sayi2 + " " + sayi3 + " " + sayi4 + " " + sayi5 + " " + sayi6)
+
+// conditionals
+// var not = 50
+// if (not > 50) {
+    // console.log("Geçtiniz")
+// }
+// else if (not = 50) {
+    // console.log("Şartlı geçtiniz")
+// }
+// else {
+    // console.log("Kaldınız")
+// }
+
+
+// a = "Almıla"
+// console.log("Hoş geldin " + a)
+
+// var sayi1 = 10
+// var sayi2 = 10
+// var sayi3 = 10
+
+// if (sayi1 == sayi2 && sayi2 == sayi3) {
+    // console.log("Sayilar eşittir.")
+// }
+
+
+// var sayi1 = 20
+// var sayi2 = 10
+// var sayi3 = 10
+
+// if (sayi1 == sayi2 || sayi2 == sayi3) {
+    // console.log("Sayilar eşittir.")
+// }
+// var sayi = 10
+
+// while (sayi <= 10 && sayi >= 0) {
+    // console.log(sayi)
+    // sayi = sayi - 1
+// }
+
+for (var i=1;i<=10;i++)
+{
+    console.log(i)
+}
